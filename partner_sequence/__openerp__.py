b2494d6ae970f32e236e2786558e7661652e87ff
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2014-12-14 23:18:30 +0100

    [ADD] Partner and Product sequence modules

---
+++ partner_sequence/__openerp__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Partner Sequence',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """Use the standard reference field on partner for the unique partner number. Adds
+    extra sequence type: Partner and a sequence with code res.partner. As default this sequence will be
+    used to assign to partners. You can use the Partner Sequence forms to set different sequence for a country.
+    The partner number will be added to the partner just like with the Products in ODOO.""",
+    'author': 'ICTSTUDIO-ONESTEIN | André Schenkels',
+    'website': 'http://www.onestein.nl',
+    'depends': [
+        'base',        
+               ],
+    'data': ['partner_view.xml',
+             'partner_sequence.xml',
+        ],
+    'installable': True,
+    'application': True,
+    }
---
+++ product_sequence/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Product Sequence',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """Adding Product Sequence to the default_code field""",
+    'author': 'ICTSTUDIO-ONESTEIN | André Schenkels',
+    'website': 'http://www.onestein.nl',
+    'depends': [
+        'base',
+        'product',
+               ],
+    'data': ['product_sequence.xml',
+        ],
+    
+    'installable': True,
+    'application': True,
+    }
